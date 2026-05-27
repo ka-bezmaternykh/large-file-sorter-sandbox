0ecf3295380ff6ba849e6e1e3adcdf6da8fa86db
Kirill Bezmaternykh <k.bezmaternykh@cupidmedia.com>
2026-05-27 20:33:11 +0400

    feat: Update DummyFile.Generator Added a check for available space

--- DummyFile.Generator/DummyFile.Generator.Tests/FilesCheckerTests.cs
+++ DummyFile.Generator/DummyFile.Generator.Tests/FilesCheckerTests.cs
@@ -65,4 +65,39 @@ public class FilesCheckerTests
             File.Delete(filePath);
         }
     }
+
+    [Fact]
+    public void TryValidate_ShouldPassWhenThereIsEnoughFreeSpaceForRequestedFileSize()
+    {
+        var options = new CommandLineOptions
+        {
+            File = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.txt"),
+            FileSizeBytes = 1
+        };
+
+        var success = FilesChecker.TryValidate(options, out var validationErrors);
+
+        Assert.True(success);
+        Assert.Empty(validationErrors);
+    }
+
+    [Fact]
+    public void TryValidate_ShouldFailWhenThereIsNotEnoughFreeSpaceForRequestedFileSize()
+    {
+        var filePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.txt");
+        var rootPath = Path.GetPathRoot(Path.GetFullPath(filePath))!;
+        var availableFreeSpaceBytes = new DriveInfo(rootPath).AvailableFreeSpace;
+
+        var options = new CommandLineOptions
+        {
+            File = filePath,
+            FileSizeBytes = availableFreeSpaceBytes
+        };
+
+        var success = FilesChecker.TryValidate(options, out var validationErrors);
+
+        Assert.False(success);
+        Assert.Single(validationErrors);
+        Assert.Contains("Not enough free disk space.", validationErrors[0], StringComparison.Ordinal);
+    }
 }
